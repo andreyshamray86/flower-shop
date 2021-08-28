58aeb8a03f68b35bc063a19d45ccabd28136d9e3
Andrey <andreyshamray86@gmail.com>
2021-08-28 14:06:59 +0300

    init firebase auth

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { SignInWithGoogle } from '../../services/firebase';
+
 import RegisterStyled from './Register.styles';
 
 const Register = () => {
@@ -8,18 +10,22 @@ const Register = () => {
             <h2 className='register__title'>New customer</h2>
             <form className='register__form'>
                 <label htmlFor="first">First Name</label>
-                <input type="text" name="" id="first" />
+                    <input type="text" name="" id="first" />
                 <label htmlFor="last">Last Name</label>
-                <input type="text" name="" id="last" />
+                    <input type="text" name="" id="last" />
                 <label htmlFor="email">Email</label>
-                <input type="email" name="" id="email" />
+                    <input type="email" name="" id="email" />
                 <label htmlFor="phone">Phone</label>
-                <input type="tel" name="" id="phone" />
+                    <input type="tel" name="" id="phone" />
                 <label htmlFor="password">Password</label>
-                <input type="password" name="" id="password" />
+                    <input type="password" name="" id="password" />
                 <label htmlFor="password2">Confirm password</label>
-                <input type="password" name="" id="password2" />
+                    <input type="password" name="" id="password2" />
                 <button className='register__form-btn'>Sign Up</button>
+                <button 
+                    className='register__form-btngoogle'
+                    onClick={SignInWithGoogle}
+                >Sign Up with Google</button>
             </form>
         </RegisterStyled>
     )
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -13,5 +14,11 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const SignInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
